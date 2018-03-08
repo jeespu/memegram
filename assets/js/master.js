26bb9f685bc5db37d08604a405d192d5ad49a40c
Esa-Pekka Autio <L3005@student.jamk.fi>
2018-03-08 15:34:45 +0200

    Front Page template added and git fixing

---
+++ assets/js/master.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   $("#sign-up").click(function(){
+      $("#sign-form").css("visibility", "visible");
+      $("#sign-form").css("opacity", "1");
+   });
+
+   $("login").click(function(){
+      $("#login-form").css("visibility", "visible");
+   })
+});
