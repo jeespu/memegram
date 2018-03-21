3903c64b8942c4a30c970c1063337381704f7387
Esa-Pekka Autio <L3005@student.jamk.fi>
2018-03-21 11:17:41 +0200

    minor tweaks to .js

--- assets/js/master.js
+++ assets/js/master.js
@@ -18,5 +18,12 @@ $(document).ready(function () {
 		$(".meme-container").html('<img class="meme-img" src="https://fthmb.tqn.com/onZS-nRlttC_o-4JSQEXImdfL3E=/768x0/filters:no_upscale()/success-56a9fd1f3df78cf772abee09.jpg">');	
 	})
 
+	$(window).on("scroll", function(){
+		var height = window.innerHeight;
+		if (window.pageYOffset >= height) {
+			console.log("Welcome to the bottom");
+			$(".meme-row:last-of-type").css("display", "none");
+		}
+	})
 	
 });
