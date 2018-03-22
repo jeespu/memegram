fb8bdd7c1114ad2d9253cd6d27a1e414a72381cb
joosia <30998472+joosia@users.noreply.github.com>
2018-03-23 01:22:51 +0200

    testing .php

---
+++ register.php
@@ -0,0 +1,24 @@
+ <?php
+$servername = " den1.mysql6.gear.host ";
+$username = "memesite";
+$password = "Vp4F7y!3!0b9";
+$dbname = "memesite";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO user (username, email, password)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
