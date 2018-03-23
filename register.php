5c97d7667aed4dd6d517770f000d58978ec44ae1
joosia <30998472+joosia@users.noreply.github.com>
2018-03-23 03:04:54 +0200

    lalalalalaltortortleasdf

--- register.php
+++ register.php
@@ -1,22 +1,22 @@
 <?php
 $servername = "den1.mysql6.gear.host";
-$serverusername = "memesite";
-$serverpassword = "Vp4F7y!3!0b9";
+$username = "memesite";
+$password = "Vp4F7y!3!0b9";
 $dbname = "memesite";
 
-$username = $_REQUEST["username"];
+$user = $_REQUEST["username"];
 $email = $_REQUEST["email"];
-$password = $_REQUEST["password"];
+$pass = $_REQUEST["password"];
 
 // Create connection
-$conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO user (username, email, password)
-VALUES ($username, $email , $password)";
+VALUES ($user, $email, $pass)";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
