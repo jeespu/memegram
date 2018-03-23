bc3e9ab0c5830ee873b823d909dda07370ae0946
joosia <30998472+joosia@users.noreply.github.com>
2018-03-23 02:47:39 +0200

    testing

--- register.php
+++ register.php
@@ -4,6 +4,10 @@ $username = "memesite";
 $password = "Vp4F7y!3!0b9";
 $dbname = "memesite";
 
+$user = $_REQUEST["username"];
+$email = $_REQUEST["email"];
+$name = $_REQUEST["name"];
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -12,7 +16,7 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO user (username, email, password)
-VALUES (username, email, password)";
+VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
