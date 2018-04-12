2b96cf15a5958de055d2d46e16ca34b6a0b7e4f2
joosia <30998472+joosia@users.noreply.github.com>
2018-04-12 14:30:26 +0300

    Latest version

---
+++ comment.php
@@ -0,0 +1,31 @@
+<?php
+   session_start();
+   // Connect to database
+   require "connect.php";
+   $conn->set_charset("utf8mb4");
+
+   $comment = input($_POST['comment']);
+   $postID = $_POST['postID'];
+   $user = $_SESSION['userID'];
+
+   $sql = "INSERT INTO comment (content, whoCommented, whichPost) VALUES ('$comment', '$user', ' $postID')";
+
+   if ($conn->query($sql) === TRUE) {
+      // Redirect to Feed -page
+      header("Location: feed.php");
+      exit();
+   } else {
+      // If error occurs
+      echo "Error: " . $sql . "<br>" . $conn->error;
+      die();
+   }
+   // Close connection
+   $conn->close();
+
+function input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
